e19206957aa5c486678debfc545b7ae24ffab710
sadikmolla-01 <sadikmolla017.com>
2024-03-12 15:41:56 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,105 @@
+// 0 bade onno je kono shonkha ke javascript 'true' hisebe dhore ney. ar 0 ke 'false' dhore. example:
+
+/** 
+
+const num = -40
+
+if (num) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+ */
+
+// Empty string chara shob string ei truth ar empty string false
+
+/**
+ * 
+ * 
+const names = 'sadik'
+
+if (names) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+ * 
+ */
+
+// Undifined hole false hobe
+
+/**
+ * 
+ * 
+let names;
+console.log(names)
+if (names) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+ */
+
+// null hole false hobe
+
+/**
+ * 
+let names = null
+
+console.log(names)
+
+if (names) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+ */
+
+
+// NaN hole false hobe
+
+/**
+ * 
+let names = NaN
+
+console.log(names)
+
+if (names) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+*/
+
+// empty array [] and empty object {} hole true hobe
+
+let array = []
+let object = {}
+
+console.log(array,object)
+
+if (array, object) {
+    console.log('contidition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+
+
+
